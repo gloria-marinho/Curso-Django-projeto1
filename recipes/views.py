e926cbf60d46c9106c05e4d97717266d0aae7346
Gloria <gloria.barbosa.marinho@gmail.com>
2023-03-07 16:49:01 -0400

    continução

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request,'recipes/home.html', context={
         'name':'gloria',
     })
-
-def contato(request):
-    return render(request, 'temp.html')
-def sobre(request):
-    return HttpResponse('sobre')
 
