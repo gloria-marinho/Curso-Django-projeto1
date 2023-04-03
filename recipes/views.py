7c6db89f6e76da89b64c480314619d376f8d18d9
Gloria <gloria.barbosa.marinho@gmail.com>
2023-04-03 16:32:36 -0400

    correções no category é no models

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,9 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -14,9 +16,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
@@ -25,7 +28,4 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
-})
-
-def search(request):
-    pass
+    })
