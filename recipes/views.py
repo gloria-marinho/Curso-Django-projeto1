52b347a6ee15b03974f04e742f87dadb830d1f5c
Gloria <gloria.barbosa.marinho@gmail.com>
2023-04-04 16:19:12 -0400

    em fase de correção de erros

--- recipes/views.py
+++ recipes/views.py
@@ -26,6 +26,6 @@ def category(request, category_id):
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipes': make_recipe(),
         'is_detail_page': True,
     })
