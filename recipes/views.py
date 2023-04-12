1c24da542e7f458d4db23b73da14b1d16c3524ce
Gloria <gloria.barbosa.marinho@gmail.com>
2023-04-12 17:25:07 -0400

    altereções no admin

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
-recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={
+   recipes = Recipe.objects.filter(is_puplished=True).order_by('-id') 
+   return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
