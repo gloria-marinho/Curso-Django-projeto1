5774d3da2d335ce7cc0299bc42422138a920fae7
Gloria <gloria.barbosa.marinho@gmail.com>
2023-04-13 17:14:49 -0400

    corregindo o autor nulo na category

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    
     def __str__(self):
         return self.name
     
--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,15 @@ from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 def home(request):
-   recipes = Recipe.objects.filter(is_puplished=True).order_by('-id') 
+   recipes = Recipe.objects.filter(is_published=True).order_by('-id') 
    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
-
+def category(request, category_id):
+   recipes = Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id') 
+   return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
