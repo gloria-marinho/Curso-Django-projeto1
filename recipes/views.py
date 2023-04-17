98c827860ad6a3d4c4ba5b94e98fe26c8670325d
Gloria <gloria.barbosa.marinho@gmail.com>
2023-04-17 18:44:53 -0400

    na introdução de testes unitários

--- recipes/views.py
+++ recipes/views.py
@@ -1,32 +1,39 @@
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
+
 def home(request):
    recipes = Recipe.objects.filter(
-      is_published=True
+      is_published=True,
       ).order_by('-id') 
+   
    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
+
+
 def category(request, category_id):
    recipes = get_list_or_404(
-      Recipe, category_id=category_id,
+      Recipe. objects.filter( 
+      category_id=category_id,
       is_published=True,
       ).order_by('-id')
-   
+   )
+
    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category_name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
+
+
 def recipe(request, id):
-    recipes = Recipe.objects.filter(
-       pk=id,
-      is_published=True
-      ).order_by('-id')-first() 
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
+    
