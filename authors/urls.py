377ad42891f5fad5472f702d915693a2267a77f8
Gloria <gloria.barbosa.marinho@gmail.com>
2023-05-03 18:44:19 -0400

    Eviando POST para outra URL e usando SESSION para salvar valores entre requests

--- authors/urls.py
+++ authors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
     path('register/, views.regoster_view, name='register'),
+    path('register/create/', views.register_create, name='create'),   
 ]
--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import  include, path
@@ -23,6 +24,6 @@ urlpatterns = [
     path('authors/', include('authors.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
