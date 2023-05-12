33b32a184bd42afbe117f87097683db641759bb1
Gloria <gloria.barbosa.marinho@gmail.com>
2023-05-12 17:25:51 -0400

    Toques finais no layout do formulário de criação de receitas

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from authors.forms.recipe_from import AuthorRecipeForm
+
 from .forms import LoginForm, RegisterForm
 
 
@@ -82,3 +84,46 @@ def logout_view(request):
     messages.error(request, 'Invalid logout request')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        context={
+            'recipes': recipes,
+        }
+    )
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+    
+    
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
+
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+    
+    'form': form
+        }
+    )
