ee63439b810181a8a3e17061fea5635a8b2c00a8
Gloria <gloria.barbosa.marinho@gmail.com>
2023-05-24 15:01:02 -0400

    Adicionando Configurações paa o Simple JWT

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,35 +1,62 @@
+from unicodedata import category
+
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-@api_view()
-def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(
-        instance=recipes,
-        many=True,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 2
 
 
-@api_view()
-def recipe_api_detail(request, pk):
-    recipe = get_object_or_404(
-        Recipe.objects.get_published(),
-        pk=pk
-    )
-    serializer = RecipeSerializer(
-        instance=recipe,
-        many=False,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+class RecipeAPIv2ViewSet(ModelViewSet):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["example"] = 'this is in context now'
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+
+    def partial_update(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+
+        recipe = self.get_queryset().filter(pk=pk).first()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
 
 
 @api_view()
